a760c678faefde0275af1a1cc45dd295e1394f80
Anton Buslavsky <buslavskii@gmail.com>
2019-11-06 15:05:21 -0800

    Double overflow file is added.

---
+++ hw2/doubleOverflow.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+
+int main(){
+
+double x = DBL_MAX;
+
+printf("x initially was: %f \n", x);
+
+x = x*2;
+
+printf(" x now is: %f \n", x);
+
+return 0;
+
+}
