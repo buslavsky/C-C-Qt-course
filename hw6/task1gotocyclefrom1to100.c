2d7963ef0dae573c7a6a98956eeb4a41ad4804d2
abuslavsky <abuslavsky@stc-spb.ru>
2019-11-07 15:45:51 +0300

    Cycle from 1 to 100 made with goto implemented.

---
+++ hw6/task1gotocyclefrom1to100.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    int i = 0;
+    label:
+    if(i<=99)
+    {++i;
+    printf("%d\n",i);
+    goto label;
+    }
+
+  return 0;
+}
